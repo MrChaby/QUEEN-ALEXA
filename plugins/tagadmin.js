6f5e87f95d90bd903f99d5f014fec903c20d3bbb
En Cuzier <89731066+En-Cuzier@users.noreply.github.com>
2021-09-26 17:20:17 +0530

    Update tagadmin.js

--- plugins/tagadmin.js
+++ plugins/tagadmin.js
@@ -17,7 +17,7 @@ if (Config.WORKTYPE == 'private') {
         mesaj = '';
         grup['participants'].map(async (uye) => {
             if (uye.isAdmin) {
-                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                mesaj += '🧞‍♀️@' + uye.id.split('@')[0] + ' ';
                 jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
             }
         });
@@ -31,7 +31,7 @@ else if (Config.WORKTYPE == 'public') {
         mesaj = '';
         grup['participants'].map(async (uye) => {
             if (uye.isAdmin) {
-                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                mesaj += '🧞‍♀️@' + uye.id.split('@')[0] + ' ';
                 jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
             }
         });
